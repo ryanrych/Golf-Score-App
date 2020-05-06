e3096f3fbf4ba6a39fed18ff806650b88df05c8d
ryanrych <ryanjrychlak@gmail.com>
2020-05-06 04:08:50 -0500

    Added dynamic plot-adding functionality to the graph on the main screen.

--- main.py
+++ main.py
@@ -10,11 +10,18 @@ from kivy.factory import Factory
 from kivy.clock import Clock
 from kivy_garden.graph import Graph, LinePlot
 
+from math import ceil
+
+userData = {}
+currUser = ""
 
 class WindowManager(ScreenManager):
     pass
 
 class LoginButtons(Widget):
+    global currUser
+    global userData
+
     userField = ObjectProperty(None)
     passwordField = ObjectProperty(None)
     loginFailed = ObjectProperty(None)
@@ -24,9 +31,10 @@ class LoginButtons(Widget):
         username = self.userField.text.lower()
         password = self.passwordField.text
 
-        if (username in GolfApp.userData):
-            if (password == GolfApp.userData[username]["password"]):
+        if (username in userData):
+            if (password == userData[username]["password"]):
                 self.loginPassed = True
+                currUser = username
             else:
                 self.loginFailedStart()
                 Clock.schedule_once(self.loginFailedEnd, 3)
@@ -59,13 +67,13 @@ class CreateAccountButtons(Widget):
         password = self.passwordField.text
         confirm = self.confirmField.text
 
-        if (username in GolfApp.userData):
+        if (username in userData):
             self.userFailStart()
         elif (password != confirm):
             self.passwordFailStart()
         else:
-            GolfApp.userData[username] = {}
-            GolfApp.userData[username]["password"] = password
+            userData[username] = {}
+            userData[username]["password"] = password
             self.userField.text = ""
             self.passwordField.text = ""
             self.confirmField.text = ""
@@ -91,7 +99,67 @@ class CreateAccountScreen(Screen):
     pass
 
 class MainButtons(Widget):
-    mainGraph = ObjectProperty(LinePlot(color = [0,138,230,1]))
+    global currUser
+    global userData
+
+    mainGraph = ObjectProperty(None)
+
+    def graphButtons(self, amount):
+        self.mainGraph.tick_color = [0, 0, 0, 1]
+        self.mainGraph._y_grid_label = range(100)
+        self.mainGraph.padding = 5
+        self.mainGraph.font_color = [0, 0, 0, 1]
+
+        plot = LinePlot(color = [1,0,1,1])
+        if (amount == 1):
+            scores = userData[currUser]["scores"][-5:]
+            points = scores
+            self.mainGraph.xmin = 0
+            self.mainGraph.xmax = 6
+            self.mainGraph.x_ticks_major = 1
+            r = max(scores) - min(scores)
+            yTicks = ceil(r / 5)
+            if (yTicks == 0): yTicks = 1
+            self.mainGraph.ymin = min(scores) - yTicks
+            self.mainGraph.ymax = max(scores) + yTicks
+            self.mainGraph.y_ticks_major = yTicks
+
+        elif (amount == 2):
+            scores = userData[currUser]["scores"][-10:]
+            points = scores
+            self.mainGraph.xmin = 0
+            self.mainGraph.xmax = 11
+            self.mainGraph.x_ticks_major = 1
+            r = max(scores) - min(scores)
+            yTicks = ceil(r / 10)
+            if (yTicks == 0): yTicks = 1
+            self.mainGraph.ymin = min(scores) - yTicks
+            self.mainGraph.ymax = max(scores) + yTicks
+            self.mainGraph.y_ticks_major = yTicks
+        else:
+            scores = userData[currUser]["scores"]
+            points = scores
+            self.mainGraph.xmin = 0
+            self.mainGraph.xmax = len(scores) + 1
+            self.mainGraph.x_ticks_major = 1
+            r = max(scores) - min(scores)
+            yTicks = ceil(r / len(scores))
+            if (yTicks == 0): yTicks = 1
+            self.mainGraph.ymin = min(scores) - yTicks
+            self.mainGraph.ymax = max(scores) + yTicks
+            self.mainGraph.y_ticks_major = yTicks
+
+        plot.points.append((0,points[0]))
+        for i in range(1,len(points)+1):
+            plot.points.append((i,points[i-1]))
+        plot.points.append((len(points)+1,points[-1]))
+
+        for x in self.mainGraph.plots:
+            self.mainGraph.remove_plot(x)
+
+        self.mainGraph.add_plot(plot)
+
+
 
 class MainBackground(Widget):
     pass
@@ -102,6 +170,8 @@ class MainScreen(Screen):
 
 
 class GolfApp(App):
+    global userData
+
     def hexToKivyColor(Object, hex, alpha):
         values = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'a': 10, 'b': 11,
                   'c': 12,
@@ -122,10 +192,11 @@ class GolfApp(App):
     users = open("Users.txt", "r")
     for user in users:
         data = user.split(",")
-        userData[data[0].lower()]={}
+        userData[data[0].lower()] = {}
         userData[data[0]]["password"] = data[1]
+        scores = list(map(int,data[2][1:-1].split(";")))
+        userData[data[0]]["scores"] = scores
     users.close()
-
 
     def build(self):
         Window.size=(350,600)
